d1d8d294f9983d8a09399c1f1b62f58775e98bca
Alejandro <alejandro.chico.lopez@alumnojoyfe.iepgroup.es>
2026-05-12 22:16:28 +0200

    Config MongoDB y estructura Lessons en NestJS

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,25 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { LessonsModule } from './lessons/lessons.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+
+    MongooseModule.forRootAsync({
+      useFactory: () => ({
+        uri: process.env.MONGO_URL,
+      }),
+    }),
+
+    LessonsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
